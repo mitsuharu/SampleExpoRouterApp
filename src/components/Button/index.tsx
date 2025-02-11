979019ef4584abba3a955736121f3bd6b1f924e0
Mitsuharu Emoto <mthr1982@gmail.com>
2025-02-11 13:31:48 +0900

    各種コンポーネントを追加した

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,75 @@
+import { styleType } from '@/utils/styles'
+import React, { ReactNode } from 'react'
+import {
+  Pressable,
+  Text,
+  ViewStyle,
+  StyleSheet,
+  TextStyle,
+  Insets,
+  StyleProp,
+} from 'react-native'
+
+type ContentProps = {
+  text?: string
+  textStyle?: StyleProp<TextStyle>
+  children?: ReactNode
+}
+type Props = ContentProps & {
+  onPress?: () => void
+  onLongPress?: () => void
+  style?: StyleProp<ViewStyle>
+  inactive?: boolean
+  accessibilityLabel?: string
+  hitSlop?: Insets
+}
+type ComponentProps = Props & {}
+
+const Component: React.FC<ComponentProps> = ({
+  text,
+  children,
+  onPress,
+  onLongPress,
+  style,
+  textStyle,
+  inactive,
+  accessibilityLabel,
+  hitSlop,
+}) => {
+  return (
+    <Pressable
+      style={({ pressed }) => [
+        styles.container,
+        style,
+        !!onPress && pressed && styles.pressed,
+        inactive && styles.inactive,
+      ]}
+      onPress={() => !inactive && onPress?.()}
+      onLongPress={() => !inactive && onLongPress?.()}
+      accessibilityLabel={accessibilityLabel}
+      accessibilityRole="button"
+      hitSlop={hitSlop}
+    >
+      {!!text && <Text style={textStyle}>{text}</Text>}
+      {!!children && children}
+    </Pressable>
+  )
+}
+
+const Container: React.FC<Props> = (props) => {
+  return <Component {...props} />
+}
+
+export { Container as Button }
+
+const styles = StyleSheet.create({
+  container: styleType<ViewStyle>({
+    opacity: 1.0,
+  }),
+  pressed: styleType<ViewStyle>({
+    opacity: 0.7,
+  }),
+  inactive: styleType<ViewStyle>({
+    opacity: 0.3,
+  }),
+})
